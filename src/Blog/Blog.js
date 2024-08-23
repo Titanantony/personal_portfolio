c68afccb6d1a7d84c188bc9d9d4dec425760ae1c
Titan antony <antonytitan96@gmail.com>
2024-08-23 15:23:25 +0300

    Chore: Separate the common files for them to be rendered to different pages

--- src/Blog/Blog.js
+++ src/Blog/Blog.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { collection, getDocs, doc, getDoc } from 'firebase/firestore'
 import { db } from './Firebase'
-import './styles/app.css'
-import SocialLinks from './SocialLinks'
+import './Blog.css'
+import Footer from '../SharedComponents/Footer'
 
 const Blog = () => {
   const [featuredArticle, setFeaturedArticle] = useState(null)
@@ -152,42 +152,7 @@ const Blog = () => {
           </div> */}
         </div>
       </main>
-
-      {/* Footer */}
-      <footer className='footer'>
-        <div className='footer-container'>
-          <div className='footer-left'>
-            <h2>Let’s Connect Here</h2>
-
-            <SocialLinks />
-            <p>
-              Feel free to connect with me on the above social media platforms.
-              I'm here to engage, collaborate, and share exciting ideas!
-            </p>
-            <p className='logo'>Antony</p>
-          </div>
-          <div className='footer-right'>
-            <div className='footer-contact'>
-              <h4>Contact</h4>
-              <p>
-                Phone: <a href='tel:+254115423788'>+254 115 423 788</a>
-              </p>
-              <p>
-                <a href='mailto:antonytitan96@.com'>antonytitan96@gmail.com</a>
-              </p>
-              <p>
-                <a
-                  href='https:/antonykinuthia.xyz'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  https:/antonykinuthia.xyz
-                </a>
-              </p>
-            </div>
-          </div>
-        </div>
-      </footer>
+      <Footer />
     </div>
   )
 }
